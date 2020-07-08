49bee1fe66f3baee036dcc20315a7da363ad4bd1
Gregorius Eldwin Pradipta <gregeld96@gmail.com>
2020-07-08 19:53:47 +0700

    Update app.js and package.json

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = 4000;
 const router = require('./routers')
 const errhandler = require('./middlewares/errorHandler')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}));
 
