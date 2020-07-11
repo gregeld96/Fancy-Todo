3a491d1da73e468a814a4867f21797d27437bcde
Gregorius Eldwin Pradipta <gregeld96@gmail.com>
2020-07-11 16:31:52 +0700

    Fix minor bug on Boredom Router

--- server/routers/boredom.js
+++ server/routers/boredom.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const controller = require('../controllers/boredom');
+const authetication = require('../middlewares/authentication');
 
+router.use(authetication);
 router.get('/activity', controller.findActivity);
 router.get('/activity/:type', controller.activityType)
 
